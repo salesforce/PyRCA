4ebb5d23f7cfb14da133ac8408b5f4264d90817a
Chenghao Liu <chenghao.liu@salesforce.com>
2023-04-26 14:09:41 +0800

    simulation data generation and its test

---
+++ tests/tools/test_simulation.py
@@ -0,0 +1,54 @@
+#
+# Copyright (c) 2023 salesforce.com, inc.
+# All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+# For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause#
+import unittest
+from pyrca.tools.simulation.data_gen import DagGenConfig,DataGenConfig,AnomalyDataGenConfig, DAGGen, DataGen, AnomalyDataGen
+
+class Testdatagen(unittest.TestCase):
+    def setUp(self):
+        dag = DAGGen(DagGenConfig)
+        self.graph = dag.gen()
+
+    def test_dag(self):
+        #print(self.graph)
+        self.assertEqual(self.graph.shape[0], 20)
+
+    def test_datagen(self):
+        try:
+            DataGenConfig(dag=self.graph, noise_type='a')
+        except AssertionError as error:
+            self.assertTrue(len(str(error)) > 0)
+
+        config = DataGenConfig(dag=self.graph, func_type='square', weight_generator='normal')
+        self.assertEqual(config.func_type(10), 100)
+
+        data, parent_weights, noise_weights, _, _, = DataGen(config).gen()
+        self.assertEqual(data.shape[1], parent_weights.shape[0])
+
+    def test_abnormaldatagen(self):
+        try:
+            AnomalyDataGenConfig()
+        except AssertionError as error:
+            self.assertTrue(len(str(error)) > 0)
+
+        config = DataGenConfig(dag=self.graph, func_type='square', weight_generator='normal')
+        data, parent_weights, noise_weights, func_type, noise_type = DataGen(config).gen()
+
+        # compute threshold, baseline
+        _SLI = 0
+        tau = 3
+        baseline = data[_SLI, :].mean()
+        sli_sigma = data[_SLI, :].std()
+        threshold = tau * sli_sigma
+
+        # generate anomaly data
+        config = AnomalyDataGenConfig(parent_weights=parent_weights, noise_weights=noise_weights, threshold=threshold,
+                                       func_type=func_type, noise_type=noise_type, baseline=baseline, anomaly_type=0)
+        anomaly_data, fault = AnomalyDataGen(config).gen_anomaly()
+        self.assertEqual(fault.shape[0], parent_weights.shape[0])
+
+
+if __name__ == "__main__":
+    unittest.main()
