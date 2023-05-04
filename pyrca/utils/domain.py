a27d61eb857cf373c2a4ef55527dd47c92e4d809
yangwenzhuo08 <wenzhuo.yang@salesforce.com>
2023-05-04 11:37:18 +0800

    Fix a bug in domain parser

--- pyrca/utils/domain.py
+++ pyrca/utils/domain.py
@@ -50,30 +50,31 @@ class DomainParser:
             except SchemaError as e:
                 raise RuntimeError("The domain knowledge config does not fit the required schema.") from e
 
-    def get_forbid_links(self, graph_nodes=None):
+    def get_forbid_links(self, graph_nodes=None, process_root_leaf=True):
         if self.config is None or "causal-graph" not in self.config:
-            return None
+            return []
 
         other_forbids = []
-        # Constraints for leaf nodes
-        leaf_nodes = self.config["causal-graph"]["leaf-nodes"]
-        if leaf_nodes is not None:
-            assert graph_nodes is not None, "`graph_nodes` cannot be None."
-            for u in leaf_nodes:
-                assert u in graph_nodes, f"{u} is not in `all_nodes`"
-                for v in graph_nodes:
-                    if u != v:
-                        other_forbids.append([u, v])
+        if process_root_leaf:
+            # Constraints for leaf nodes
+            leaf_nodes = self.config["causal-graph"]["leaf-nodes"]
+            if leaf_nodes is not None:
+                assert graph_nodes is not None, "`graph_nodes` cannot be None."
+                for u in leaf_nodes:
+                    assert u in graph_nodes, f"{u} is not in `all_nodes`"
+                    for v in graph_nodes:
+                        if u != v:
+                            other_forbids.append([u, v])
 
-        # Constraints for root nodes
-        root_nodes = self.config["causal-graph"]["root-nodes"]
-        if root_nodes is not None:
-            assert graph_nodes is not None, "`graph_nodes` cannot be None."
-            for u in root_nodes:
-                assert u in graph_nodes, f"{u} is not in `all_nodes`"
-                for v in graph_nodes:
-                    if u != v:
-                        other_forbids.append([v, u])
+            # Constraints for root nodes
+            root_nodes = self.config["causal-graph"]["root-nodes"]
+            if root_nodes is not None:
+                assert graph_nodes is not None, "`graph_nodes` cannot be None."
+                for u in root_nodes:
+                    assert u in graph_nodes, f"{u} is not in `all_nodes`"
+                    for v in graph_nodes:
+                        if u != v:
+                            other_forbids.append([v, u])
 
         # Additional forbidden links
         forbids = self.config["causal-graph"]["forbids"]
@@ -94,6 +95,20 @@ class DomainParser:
 
     def get_metrics(self):
         return None if self.config is None or "metrics" not in self.config else self.config["metrics"]
+
+    def get_root_nodes(self):
+        return (
+            []
+            if self.config is None or "causal-graph" not in self.config
+            else self.config["causal-graph"]["root-nodes"]
+        )
+
+    def get_leaf_nodes(self):
+        return (
+            []
+            if self.config is None or "causal-graph" not in self.config
+            else self.config["causal-graph"]["leaf-nodes"]
+        )
 
     def print(self):
         pprint.pprint(self.config)
